ae55a9a602ebbb2ddcdefbcbd3c31a5e9183f534
irmakkucukoba <152858534+irmakkucukoba@users.noreply.github.com>
2025-05-15 20:38:02 +0300

    firestore_config.py

--- backend/firestore_config.py
+++ backend/firestore_config.py
@@ -1,8 +1,10 @@
+import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/app/thermal-imprint-459211-c7-500ba9fb2a6d.json"
+
 from google.cloud import firestore
 
 db = firestore.Client()
 
-# Kolay erişim için referanslar
 users = db.collection("users")
 singers = db.collection("singers")
 albums = db.collection("albums")
