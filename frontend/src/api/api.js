f98adb7d31aada6d343a708263025e50003151cc
irmakkucukoba <152858534+irmakkucukoba@users.noreply.github.com>
2025-05-16 21:51:54 +0300

    api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:8000";
+export const API_BASE = "http://35.202.164.153"; 
 
 export const fetchSongs = () => axios.get(`${API_BASE}/songs`);
 
@@ -11,10 +11,8 @@ export const rateSong = (proID, songID, rate) =>
     rate,
   });
 
-
-  
 export const login = (nick, password) =>
   axios.post(`${API_BASE}/login`, { nick, password });
-  
+
 export const signup = (nick, password) =>
   axios.post(`${API_BASE}/signup`, { nick, password });
