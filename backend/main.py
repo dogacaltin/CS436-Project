40b9f6f3dc89648e80f864fcf678cd5a12327d3c
irmakkucukoba <152858534+irmakkucukoba@users.noreply.github.com>
2025-05-17 17:35:34 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -167,4 +167,4 @@ def get_singer(sid: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8080)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
